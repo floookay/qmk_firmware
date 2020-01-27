44e48c74218449839ad26f843b0992c8acc4785f
floookay <developer@floookay.de>
2020-01-27 20:47:23 +0100

    updated keymap according to usage

--- keyboards/arisu/keymaps/default/keymap.c
+++ keyboards/arisu/keymaps/default/keymap.c
@@ -17,58 +17,55 @@
 
 enum arisu_layers {
   _QWERTY,
-  _GAMING,
   _FUNCTION
 };
 
 // Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
-  GAMING,
   FUNCTION
 };
 
 // Tap Dance Declarations
 enum {
-    TD_LALT_LGUI = 0,
-    TD_RALT_RGUI
+    _TD_PGUP = 0,
+    _TD_PGDN = 1,
+    _TD_HOME = 2,
+    _TD_END = 3
 };
 
 #define MO_FN MO(_FUNCTION)
-#define TG_GAME TG(_GAMING)
-#define LALT_LG TD(TD_LALT_LGUI)
-#define RALT_RG TD(TD_RALT_RGUI)
+#define SPC_FN LT(_FUNCTION, KC_SPC)
+#define TD_PGUP TD(_TD_PGUP)
+#define TD_PGDN TD(_TD_PGDN)
+#define TD_HOME TD(_TD_HOME)
+#define TD_END TD(_TD_END)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( /* Base */
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_GRV,  KC_BSLS, KC_DEL, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,          KC_HOME, \
-    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                    KC_END,  \
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV, KC_DEL,  \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,         TD_PGUP, \
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                   TD_PGDN, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,                     \
-    KC_LCTL,          LALT_LG,          MO_FN,   MO_FN,            KC_SPC,           KC_RALT,                   KC_LEFT, KC_DOWN, KC_RGHT           \
-  ),
-
-  // to be toggled on or off
-  [_GAMING] = LAYOUT(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, \
-    KC_RALT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,                   \
-    _______,          KC_LALT,          KC_SPC,  MO_FN,            _______,          _______,                   _______, _______, _______           \
+    KC_LALT,          KC_LGUI,          KC_SPC,  MO_FN,            KC_SPC,           KC_RALT,                   KC_LEFT, KC_DOWN, KC_RGHT           \
   ),
 
   // a momentary layer
   [_FUNCTION] = LAYOUT(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_INS,  \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,           KC_PGUP, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, TG_GAME,                   KC_PGDN, \
+    _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______, _______, KC_DEL,           TD_HOME, \
+    _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, KC_MPLY,                   TD_END,  \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_VOLU,                   \
-    _______,          _______,          _______, _______,          KC_MPLY,          _______,                   KC_MPRV, KC_VOLD, KC_MNXT           \
+    _______,          _______,          _______, _______,          _______,          _______,                   KC_MPRV, KC_VOLD, KC_MNXT           \
   )  
 };
+
 
 // Tap Dance Definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
     // Tap once for L-Alt, twice for L-GUI
-    [TD_LALT_LGUI] = ACTION_TAP_DANCE_DOUBLE(KC_LALT, KC_LGUI)
+    [_TD_PGUP] = ACTION_TAP_DANCE_DOUBLE(KC_PGUP, LCTL(KC_PGUP)),
+    [_TD_PGDN] = ACTION_TAP_DANCE_DOUBLE(KC_PGDN, LCTL(KC_PGDN)),
+    [_TD_HOME] = ACTION_TAP_DANCE_DOUBLE(KC_HOME, LCTL(KC_HOME)),
+    [_TD_END]  = ACTION_TAP_DANCE_DOUBLE(KC_END,  LCTL(KC_END))
 };
