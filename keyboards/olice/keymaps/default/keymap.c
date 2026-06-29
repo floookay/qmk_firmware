0b3ab268f280d9734f9baa94b40baa5e16393a05
floookay <info@floookay.de>
2026-06-29 14:55:34 +0200

    fixed ext colums

--- keyboards/olice/keymaps/default/keymap.c
+++ keyboards/olice/keymaps/default/keymap.c
@@ -22,10 +22,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┴───┘└───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘└───┴───┴───┴───┘
      */
     [0] = LAYOUT_ortho_4x20(
-        KC_P7,   KC_P8,   KC_P9,   KC_PSLS,     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,     KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
-        KC_P4,   KC_P5,   KC_P6,   KC_PAST,     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,     KC_P4,   KC_P5,   KC_P6,   KC_PAST,
-        KC_P1,   KC_P2,   KC_P3,   KC_PMNS,     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,      KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
-        KC_P0,   KC_PDOT, KC_PENT, KC_PPLS,     KC_DEL,  KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,  KC_DEL,  KC_ENT,  MO(1),   MO(1),   MO(2),   KC_RALT, KC_ESC,      KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
+        KC_P7,   KC_P8,   KC_P9,   KC_PMNS,     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,     KC_P7,   KC_P8,   KC_P9,   KC_PMNS,
+        KC_P4,   KC_P5,   KC_P6,   KC_PPLS,     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,     KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+        KC_P1,   KC_P2,   KC_P3,   KC_PENT,     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,      KC_P1,   KC_P2,   KC_P3,   KC_PENT,
+        KC_P0,   KC_P0,   KC_PDOT, KC_PENT,     KC_DEL,  KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,  KC_DEL,  KC_ENT,  MO(1),   MO(1),   MO(2),   KC_RALT, KC_ESC,      KC_P0,   KC_P0,   KC_PDOT, KC_PENT
     ),
 
     /* numbers and symbols
