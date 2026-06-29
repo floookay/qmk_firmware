213f0cfa96de237943bb84f11a6e02d3e45e8afb
floookay <info@floookay.de>
2026-06-29 10:51:20 +0200

    updated keymap

--- keyboards/olice/keymaps/default/keymap.c
+++ keyboards/olice/keymaps/default/keymap.c
@@ -3,6 +3,11 @@
 
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+    M_ARROW = SAFE_RANGE,
+    M_THIS
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* base layer
@@ -13,14 +18,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├───┼───┼───┼───┤├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤├───┼───┼───┼───┤
      * │F5 │F6 │F7 │F8 ││Sft│ Z │ X │ C │ V │ B │ N │ M │,< │.> │/? │Sft││ 1 │ 2 │ 3 │Ent│
      * ├───┼───┼───┼───┤├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤├───┼───┼───┼───┤
-     * │F1 │F2 │F3 │F4 ││   │Alt│Gui│   │   │Del│Ent│[1]│[1]│[2]│AlG│   ││ 0 │ 0 │ . │Ent│
+     * │F1 │F2 │F3 │F4 ││   │Alt│Gui│   │   │Del│Ent│[1]│[1]│[2]│AlG│Esc││ 0 │ 0 │ . │Ent│
      * └───┴───┴───┴───┘└───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘└───┴───┴───┴───┘
      */
     [0] = LAYOUT_ortho_4x20(
-        KC_ESC,  _______, _______, _______,     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,     KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
-        _______, _______, _______, _______,     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,     KC_P4,   KC_P5,   KC_P6,   KC_PAST,
-        _______, _______, _______, _______,     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,      KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
-        _______, _______, _______, _______,     KC_GRV,  KC_LCTL, KC_LGUI, KC_LALT, MO(1),   KC_SPC,  KC_SPC,  MO(2),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,     KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
+        KC_P7,   KC_P8,   KC_P9,   KC_PSLS,     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,     KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
+        KC_P4,   KC_P5,   KC_P6,   KC_PAST,     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,     KC_P4,   KC_P5,   KC_P6,   KC_PAST,
+        KC_P1,   KC_P2,   KC_P3,   KC_PMNS,     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,      KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
+        KC_P0,   KC_PDOT, KC_PENT, KC_PPLS,     KC_DEL,  KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,  KC_DEL,  KC_ENT,  MO(1),   MO(1),   MO(2),   KC_RALT, KC_ESC,      KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
     ),
 
     /* numbers and symbols
@@ -35,10 +40,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┴───┘└───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘└───┴───┴───┴───┘
      */
     [1] = LAYOUT_ortho_4x20(
-        _______, _______, _______, _______,     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,     KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
-        _______, _______, _______, _______,     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,     KC_P4,   KC_P5,   KC_P6,   KC_PAST,
-        _______, _______, _______, _______,     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,      KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
-        _______, _______, _______, _______,     KC_GRV,  KC_LCTL, KC_LGUI, KC_LALT, MO(1),   KC_SPC,  KC_SPC,  MO(2),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,     KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
+        _______, _______, _______, _______,     M_THIS,  S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), _______,     _______, _______, _______, _______,
+        _______, _______, _______, _______,     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,     _______, _______, _______, _______,
+        _______, _______, _______, _______,     _______, KC_GRV,  KC_LPRN, KC_LBRC, KC_LCBR, KC_MINS, KC_EQL,  KC_RCBR, KC_RBRC, KC_RPRN, KC_BSLS, _______,     _______, _______, _______, _______,
+        _______, _______, _______, _______,     _______, _______, _______, M_ARROW, M_ARROW, _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______
     ),
 
     /* navigation & function
@@ -53,10 +58,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┴───┘└───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘└───┴───┴───┴───┘
      */
     [2] = LAYOUT_ortho_4x20(
-        _______, _______, _______, _______,     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,     KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
-        _______, _______, _______, _______,     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,     KC_P4,   KC_P5,   KC_P6,   KC_PAST,
-        _______, _______, _______, _______,     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,      KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
-        _______, _______, _______, _______,     KC_GRV,  KC_LCTL, KC_LGUI, KC_LALT, MO(1),   KC_SPC,  KC_SPC,  MO(2),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,     KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
+        _______, _______, _______, _______,     KC_ESC,  KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_WFWD, _______, KC_PGUP, KC_UP,   KC_PGDN, KC_PSCR, _______,     _______, _______, _______, _______,
+        _______, _______, _______, _______,     _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_WSTP, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  _______,     _______, _______, _______, _______,
+        _______, _______, _______, _______,     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_WBAK, KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, _______,     _______, _______, _______, _______,
+        _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______
     ),
 
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        switch(keycode) {
+            case M_ARROW:
+                SEND_STRING("->");
+                break;
+            case M_THIS:
+                SEND_STRING("$this->");
+                break;
+            default:
+                break;
+        }
+    }
+    return true;
+}
+
+#ifdef OLED_ENABLE
+bool oled_task_user(void) {
+    oled_write_ln_P(PSTR("olice"), false);
+    return false;
+}
+#endif
